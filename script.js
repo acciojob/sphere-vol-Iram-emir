e1b2b2ccf23916a0137226001fd10110f3be70d8
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-26 19:09:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function volume_sphere() {
     //Write your code here
-  
+	event.preventDefault(); // Prevent form  submission
+
+	let r = document.getElementById('radius').value;
+    let v = (4/3) * Math.PI * (r * r *r);
+	document.getElementById('volume').value = v.toFixed(4);
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+	document.getElementById('MyForm').addEventlistener('submit', volume_sphere);
+		};
